78e1b63e3e6343c9fc4e8f50c8b12759085aeb1c
rosi2701 <rcsfuentes@gmail.com>
2025-10-03 13:50:17 -0300

    portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -1,4 +1,5 @@
 console.log("hola");
+
 const trabajos = [
     {
         title: "Batimóvil",
@@ -42,45 +43,21 @@ const trabajos = [
     }
 ];
 
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawBasic);
 
-function drawBasic() {
-    var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population'],
-        ['New York City, NY', 8175000],
-        ['Los Angeles, CA', 3792000],
-        ['Chicago, IL', 2695000],
-        ['Houston, TX', 2099000],
-        ['Philadelphia, PA', 1526000]
-    ]);
-    
-    var options = {
-        title: 'Population of Largest U.S. Cities',
-        chartArea: {width: '50%'},
-        hAxis: {
-            title: 'Total Population',
-            minValue: 0
-        },
-        vAxis: {
-            title: 'City'
-        }
-    };
-    
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-}
 
 const galleryGrid = document.querySelector(".gallery-grid");
-trabajos.forEach(trabajo => {
-    const item = document.createElement("div");
-    item.classList.add("gallery-item");
-    item.innerHTML = `
-        <img src="${trabajo.imagen}" alt="${trabajo.title}">
-        <div class="gallery-overlay">
-            <h3>${trabajo.title}</h3>
-            <p>${trabajo.category}</p>
-        </div>
-    `;
-    galleryGrid.appendChild(item);
-});
+
+if (galleryGrid) {
+    trabajos.forEach(trabajo => {
+        const item = document.createElement("div");
+        item.classList.add("gallery-item");
+        item.innerHTML = `
+            <img src="${trabajo.imagen}" alt="${trabajo.title}">
+            <div class="gallery-overlay">
+                <h3>${trabajo.title}</h3>
+                <p>${trabajo.category}</p>
+            </div>
+        `;
+        galleryGrid.appendChild(item);
+    });
+}
